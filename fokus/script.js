42a8ed08c437444231dff35288d79d637193586b
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-23 15:09:49 -0300

    feat: aula 1

--- fokus/script.js
+++ fokus/script.js
@@ -1,2 +1,35 @@
 const buttons = document.querySelectorAll('button')
 console.log(buttons)
+
+const html = document.querySelector('html')
+
+const btnFoco = document.querySelector('.app__card-button--foco ')
+const btnFocoCurto = document.querySelector('.app__card-button--curto')
+const btnFocoLongo = document.querySelector('.app__card-button--longo')
+const appImage = document.querySelector('.app__image')
+
+btnFoco.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'foco')
+    appImage.setAttribute('src', '/fokus/imagens/foco.png')
+    btnFoco.classList.add('active')
+    btnFocoCurto.classList.remove('active')
+    btnFocoLongo.classList.remove('active')
+
+})
+btnFocoCurto.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-curto')
+    appImage.setAttribute('src', '/fokus/imagens/descanso-curto.png')
+    btnFoco.classList.remove('active')
+    btnFocoCurto.classList.add('active')
+    btnFocoLongo.classList.remove('active')
+})
+btnFocoLongo.addEventListener('click', () => {
+    html.setAttribute('data-contexto', 'descanso-longo')
+    appImage.setAttribute('src', '/fokus/imagens/descanso-longo.png')
+    btnFoco.classList.remove('active')
+    btnFocoCurto.classList.remove('active')
+    btnFocoLongo.classList.add('active')
+})
+
+
+

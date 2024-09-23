7e1c53f2c728a85ed63962101094adfc6ebddffe
PedrOliveiraM <pedropucmont@gmail.com>
2024-09-23 18:41:00 -0300

    feat: aula 3

--- fokus/script.js
+++ fokus/script.js
@@ -1,55 +1,107 @@
 const html = document.querySelector('html')
-const btnFoco = document.querySelector('.app__card-button--foco ')
-const btnFocoCurto = document.querySelector('.app__card-button--curto')
-const btnFocoLongo = document.querySelector('.app__card-button--longo')
-const appImage = document.querySelector('.app__image')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
+const musicaFocoInput = document.querySelector('#alternar-musica')
+const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3');
+const iniciarOuPausarBt = document.querySelector('#start-pause span')
 
-const title = document.querySelector('.app__title')
+const timer = document.querySelector('#timer')
 
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
-btnFoco.addEventListener('click', () => {
+musica.loop = true
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play()
+    } else {
+        musica.pause()
+    }
+})
+
+focoBt.addEventListener('click', () => {
     alterarContexto('foco')
+    focoBt.classList.add('active')
 })
-btnFocoCurto.addEventListener('click', () => {
-    alterarContexto('descanso-curto')
-})
-btnFocoLongo.addEventListener('click', () => {
-    alterarContexto('descanso-longo')
 
+curtoBt.addEventListener('click', () => {
+    alterarContexto('descanso-curto')
+    curtoBt.classList.add('active')
+})
+
+longoBt.addEventListener('click', () => {
+    alterarContexto('descanso-longo')
+    longoBt.classList.add('active')
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(function (contexto) {
+        contexto.classList.remove('active')
+    })
     html.setAttribute('data-contexto', contexto)
-    appImage.setAttribute('src', `/fokus/imagens/${contexto}.png`)
-
-
-
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
     switch (contexto) {
-        case 'foco':
-            desactivatingButtons()
-            btnFoco.classList.add('active')
-            title.innerHTML = 'Otimize sua produtividade,<br><strong strong class="app__title-strong" > mergulhe no que importa.</strong >'
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
             break;
-
-        case 'descanso-curto':
-            desactivatingButtons()
-            btnFocoCurto.classList.add('active')
-            title.innerHTML = 'Que tal dar uma respirada?<br><strong class="app__title-strong">Faça uma pausa curta!</strong>'
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
             break;
-
-        case 'descanso-longo':
-            desactivatingButtons()
-            btnFocoLongo.classList.add('active')
-            title.innerHTML = 'Hora de voltar à superfície.<br><strong class="app__title-strong">Faça uma pausa longa.</strong>'
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
             break;
     }
+}
 
-
-
-    function desactivatingButtons() {
-        const btnsCardContext = document.querySelectorAll('.app__card-button')
-        btnsCardContext.forEach(element => {
-            element.classList.remove('active')
-        });
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado!')
+        zerar()
+        return
     }
+    tempoDecorridoEmSegundos -= 1
+    mostrarTempo()
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
+    iniciarOuPausarBt.textContent = "<strong>Pausar</strong>"
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function mostrarTempo() {
+    const tempo = tempoDecorridoEmSegundos
+    timer.innerHTML = tempoDecorridoEmSegundos
+}
+
+function iniciarOuPausar() {
+    if (intervaloId) {
+        audioPausa.play()
+        zerar()
+        return
+    }
+    audioPlay.play()
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar() {
+    clearInterval(intervaloId)
+    iniciarOuPausarBt.textContent = "<strong>Começar</strong>"
+    intervaloId = null
 }
